0963059758c73da5a867e1537cbbb0ee599f3a95
shiiotaka <shiiotaka@gmail.com>
2021-01-16 22:54:36 +0900

    finish ch 08

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     # この時点では保存されない
     @user = User.new(user_params)
     if @user.save #ここで保存を行う
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
