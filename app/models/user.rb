0b282f818d5b8e3981073e85081ab87c91695002
shiiotaka <shiiotaka@gmail.com>
2021-01-18 23:38:44 +0900

    ch10

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,13 @@ class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { self.email = email.downcase }
   has_secure_password
-
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   validates :name, presence: true, length: {maximum: 50}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
